e337ac875dfced41ad5794118d90a2f39f3126cd
timka <timkaa5@yandex.ru>
2023-05-29 11:33:57 +0500

    optimize imports

--- src/main/java/com/example/dto/PersonDto.java
+++ src/main/java/com/example/dto/PersonDto.java
@@ -1,6 +1,10 @@
 package com.example.dto;
 
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.data.annotation.Transient;
+import org.springframework.validation.annotation.Validated;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -8,11 +12,10 @@ import lombok.*;
 @Setter
 @Data
 public class PersonDto {
-
+    @NotBlank
     private String username;
 
     private String password;
-
 
     private String confirmPassword;
 
